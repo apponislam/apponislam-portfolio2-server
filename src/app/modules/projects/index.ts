68a212c63caf106e20a4a7781a005cad13ee5b71
Appon Islam <appon2003@gmail.com>
2026-03-06 18:59:02 +0600

    project unused file removed

--- src/app/modules/projects/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./projects.routes";
-export { ProjectModel } from "./projects.model";
-export { projectServices } from "./projects.services";
-export { projectControllers } from "./projects.controllers";
-export * from "./projects.interface";
